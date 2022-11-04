6a4f6451fadd569cc6bcf5142a393aaaf1efc957
Gaurav Sable <Gaurav-sable>
2022-11-05 02:45:16 +0530

    Discoverjs->POP working

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,18 +5,27 @@ export const shazamCoreApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers) =>{
-            headers.set('X-RapidAPI-Key', '3f39b720b9msh6ed9195951034fbp1ddb08jsn214698bd77ef');
+            headers.set('X-RapidAPI-Key', '583649bb97msh9824755f22c5422p11e6a8jsncf247ccd99b4');
+            
             return headers;
         },
     }),  
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () =>  '/charts/world' }),   
+        getTopCharts: builder.query({ query: () => '/charts/world' }),  
+        getSongsByGenre: builder.query({ query : (genre) => `/charts/genre-world?genre_code=${genre}`}),
         getSongDetails: builder.query({query: ({ songid }) => `/tracks/details?track_id=${ songid }` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${ songid }`}),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_Id=${artistId}`}),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
     }),
 
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
 } = shazamCoreApi;
